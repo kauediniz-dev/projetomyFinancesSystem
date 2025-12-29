3bd6d327d950c1d91888fea2c59976845ebba2dd
kauediniz-dev <dinizkaue26@gmail.com>
2025-12-28 21:46:34 -0300

    fontawesome

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,14 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSackDollar } from "@fortawesome/free-solid-svg-icons";
+import "./Header.css";
+
 const Header = () => {
-    return <h2>Hello Header!</h2>;
+  return (
+    <header className="header_container">
+      <FontAwesomeIcon icon={faSackDollar}color="#7AF1a7" size="2x" />
+      <h2>My Finances</h2>
+    </header>
+  );
 };
 
 export default Header;
