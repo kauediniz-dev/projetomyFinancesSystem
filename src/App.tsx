e6aa19f92ed3f58e246caee214819b835f359221
kauediniz-dev <dinizkaue26@gmail.com>
2026-01-03 17:29:58 -0300

    movements

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,45 @@
 import './App.css'
+import { useState } from "react"
 import FinanceControl from './components/Header/FinanceControl/FinanceControl'
 import Header from './components/Header/Header'
+import type { Movement } from './models/interfaces/Movements/movement';
 
 function App() {
+  const [currentBalance, setCurrentBalance] = useState(0); // State de saldo atual
+  const [currentExpenses, setCurrentExpenses] = useState(0); // State de despesas atual
+  const [movementsItens, setMovementsItens] = useState<Array<Movement>>([]); // State de movimentações
+
+  const setNewMovement = (movement: Movement) => {
+    if(movement) {
+      setMovementsItens((prevMovements) => {
+        const movements = [...prevMovements];
+        movements.unshift({
+          name: movement.name,
+          value: movement.value,
+          type: movement.type,
+          id: Math.random().toString(),
+        })
+        return movements;
+      });
+
+      movement.type === "Input" && 
+      setCurrentBalance(
+        (prevBalace) => prevBalace + Number(movement.value)
+      );
+
+     if  (movement.type === "Output") { 
+      setCurrentExpenses(
+        (prevExpenses) => prevExpenses + Number(movement.value)
+      );
+
+      currentBalance > 0 &&
+      setCurrentBalance(
+        (prevBalance) => prevBalance - Number(movement.value)
+      );
+    
+     }
+    }
+  };
 
   return (
     <div>
---
+++ src/models/interfaces/Movements/Movement.ts
@@ -0,0 +1,7 @@
+export interface Movement {
+    name: string;
+    value: string;
+    type: "Input" | "Output";
+    id?: string
+
+};
